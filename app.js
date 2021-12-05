4de34d125483c7089f8d445af8a5c56cefeb5717
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-12-05 09:36:13 +0200

    mongodb package added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 //jshint esversion:6
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
+const uri = `mongodb+srv://joz:${process.env.DB_PASS}@butik.qrb2j.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+// database connection
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
@@ -23,12 +29,24 @@ app.get("/about", function(req, res){
   app.get("/admin", function(req, res){
     res.render("admin");
   });
-  app.get("/special", function(req, res){
-    res.render("special");
+app.get("/special", function (req, res) {
+  res.render("special");
+});
+
+app.get("/all", function (req, res) {
+  db.collection("details").find({}).toArray((err, data) => {
+    if (err) {
+      console.log("connection problem")
+    }
+    else {
+      // console.log(data);
+      res.render("allproducts", {
+        products: data
+      });
+    }
   });
-  app.get("/all", function(req, res){
-    res.render("allproducts");
-  });
+});
+
   app.get("/order", function(req, res){
     res.render("order");
   });
