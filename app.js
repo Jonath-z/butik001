f21073082c792431c8ecebda920ba666cf1b2c96
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-12-13 14:06:22 +0200

    API connected

--- app.js
+++ app.js
@@ -1,60 +1,33 @@
 //jshint esversion:6
 const express = require("express");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const home = require('./routes/home');
+const about = require('./routes/about');
+const contact = require('./routes/contact');
+const admin = require('./routes/admin');
+const special = require('./routes/special');
+const products = require('./routes/products');
+const all = require('./routes/all');
+const order = require('./routes/order');
+const command = require('./routes/commands/command');
 
 const app = express();
-const uri = `mongodb+srv://joz:${process.env.DB_PASS}@butik.qrb2j.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-// database connection
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
-
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.use('/', home);
+app.use('/about', about);
+app.use('/contact', contact);
+app.use('/admin', admin);
+app.use('/special', special);
+app.use('/product', products);
+app.use('/all', all);
+app.use('/order', order);
+app.use('/command', command);
 
-app.get("/", function(req, res){
-    res.render("home");
-  });
 
-
-app.get("/about", function(req, res){
-    res.render("about");
-  });
-
-  app.get("/contact", function(req, res){
-    res.render("contact");
-  });
-
-  app.get("/admin", function(req, res){
-    res.render("admin");
-  });
-app.get("/special", function (req, res) {
-  res.render("special");
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
-
-app.get("/all", function (req, res) {
-  db.collection("details").find({}).toArray((err, data) => {
-    if (err) {
-      console.log("connection problem")
-    }
-    else {
-      // console.log(data);
-      res.render("allproducts", {
-        products: data
-      });
-    }
-  });
-});
-
-  app.get("/order", function(req, res){
-    res.render("order");
-  });
-
-
-
-
-  app.listen(3000, function() {
-    console.log("Server started on port 3000");
-  });
   
