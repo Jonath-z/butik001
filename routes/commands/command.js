f10fbf6ab006082c192646a295dd69e9a8438953
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-12-15 17:05:08 +0200

    link modified

--- routes/commands/command.js
+++ routes/commands/command.js
@@ -6,36 +6,36 @@ const nodemailer = require('nodemailer');
 
 route.use(bodyparser.urlencoded({ extended: false }));
 
-// mail sending system
-let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    service: 'Gmail',
-    auth: {
-        user: `${process.env.EMAIL_USER}`,
-        pass: `${process.env.EMAIL_PASS}`
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-});
-    
 
-route.post("/", (req, res) => {
+route.post("/", async (req, res) => {
+
+    // mail sending system
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        requireTLS: true,
+        auth: {
+            user: `${process.env.EMAIL_USER}`,
+            pass: `${process.env.EMAIL_PASS}`
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
     //______________ send mail with defined transport object_________________//
     transporter.sendMail({
         from: `"Butik" <${process.env.EMAIL_USER}>`, // sender address
         to: [`"${req.body.email}"`, `${process.env.ADMIN_MAIL}`], // list of receivers
         subject: "Command's confirmation", // Subject line
-        html: `"<p>Dear ${req.body.name} ${req.body.lastName};<br>
+        html: `<p>Dear ${req.body.name} ${req.body.lastName};<br>
                 from ${req.body.flexRadioDisabled};<br>
                 your command is received.
-                Please send ${req.body.price}$ to +243977473567 if your are in DRC,<br>
-                to +250781980810 if your are in Rwanda <b>to confirm your command</b>,specifying the name and postname entered to the command's form.<br>
-                 <b>Thanks for trusting us, BUTIK 004</b></p>"`,// html body
+                Please send <b>${req.body.price}$</b> to +243977473567 if your are in DRC,<br>
+                to +250781980810 if your are in Rwanda <b>to confirm your command</b>,specifying the name and last-name entered to the command's form.<br>
+                 <b>Thanks for trusting us, BUTIK 004</b></p>`,// html body
         attachments: [{
-            filename: "Your command",
+            filename: `Your command`,
             path: `${req.body.url}`
         }]
     });
@@ -54,10 +54,10 @@ route.post("/", (req, res) => {
         // model: data[0].model,
         Date: today
     }
-    db.collection('commandUser').insertOne(user);
+    await db.collection('commandUser').insertOne(user);
 
     // _______REDIRECTION TO THE COMMAND TYPE____________//
-    res.redirect(`/product/${req.body.type}`);
+    res.redirect(`/product/${req.body.type.trim()}`);
 });
 
 module.exports = route;
