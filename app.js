3f3fc161b0d229afa6d15d11ec69dab7245ebba9
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-12-15 18:04:47 +0200

    PORT config

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/order', order);
 app.use('/command', command);
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log("Server started on port 3000");
 });
   
